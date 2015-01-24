bd4f39cd4d81f077a5536f68bdf20556c1376275
twarrre <tw4194@hotmail.com>
2015-01-24 11:59:01 -0800

    Added new art assets and added temporary clouds.

---
+++ ProjectBluebell/Assets/Scripts/CloudBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudBehavior : MonoBehaviour {
+
+    public GameObject world;
+
+	// Use this for initialization
+	void Start () {
+        //this.transform.parent = world.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(world.transform.position, new Vector3(0.0f, 0.0f, 1.0f), 0.02f);
+	}
+}
