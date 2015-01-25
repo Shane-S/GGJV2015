5da1e4e0371a7d2cf85270221dfd0c931a63a9b6
twarrre <tw4194@hotmail.com>
2015-01-24 18:08:55 -0800

    starting animation.

--- ProjectBluebell/Assets/Scripts/CloudBehavior.cs
+++ ProjectBluebell/Assets/Scripts/CloudBehavior.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class CloudBehavior : MonoBehaviour {
 
     public GameObject world;
+    private float bounce;
 
 	// Use this for initialization
 	void Start () {
         //this.transform.parent = world.transform;
+        bounce = 1f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {        
         transform.RotateAround(world.transform.position, new Vector3(0.0f, 0.0f, 1.0f), 0.02f);
+       
 	}
 }
