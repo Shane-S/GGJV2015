67efa92d74a755d8de184730ff26e311cbf9fba9
twarrre <tw4194@hotmail.com>
2015-01-24 16:14:35 -0800

    Added a transistion to the fail scene.

--- ProjectBluebell/Assets/Hunger.cs
+++ ProjectBluebell/Assets/Hunger.cs
@@ -4,15 +4,41 @@ using System.Collections;
 public class Hunger : MonoBehaviour {
 
     public float hunger = 0;
+    public string failState;
+    public bool fading;
 
 	// Use this for initialization
 	void Start () {
-	
+        fading = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (hunger < 100)
-            hunger += 0.05f;
+
+        if (!fading)
+        {
+            if (hunger < 100)
+                hunger += 0.05f;
+
+            if (hunger >= 100)
+            {
+                CameraFader fade = GameObject.Find("Main Camera").GetComponent<CameraFader>();
+
+                if (fade != null)
+                {
+                    fading = true;
+                    fade.FadeOut(FailGame);
+                }
+                else
+                {
+                    Debug.LogWarning("CameraFader not found");
+                }
+            }
+        }
 	}
+
+    void FailGame()
+    {
+        Application.LoadLevel(failState);
+    }
 }
